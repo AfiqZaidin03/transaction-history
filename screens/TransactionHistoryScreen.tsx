cd21913bb012216852c69a994f0e3d03c2793cf2
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-12-09 03:54:52 +0800

    [11] Add list transaction and style

--- screens/TransactionHistoryScreen.tsx
+++ screens/TransactionHistoryScreen.tsx
@@ -1,14 +1,59 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { FlatList } from "react-native-gesture-handler";
+import transactions from "@/constants/transaction.json";
 
-const TransactionHistoryScreen = () => {
+type Transaction = {
+  amount: number;
+  date: string;
+  description: string;
+  type: "debit" | "credit";
+};
+
+const TransactionHistoryScreen: React.FC = () => {
+  const renderItem = ({ item }: { item: Transaction }) => (
+    <View style={styles.item}>
+      <Text style={styles.date}>{item.date}</Text>
+      <Text style={styles.description}>{item.description}</Text>
+      <Text style={styles.amount}>
+        {item.type === "debit" ? "-" : "+"}${item.amount.toFixed(2)}
+      </Text>
+    </View>
+  );
+
   return (
     <View>
-      <Text>TransactionHistoryScreen</Text>
+      <FlatList
+        data={transactions as Transaction[]}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 };
 
 export default TransactionHistoryScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  date: {
+    flex: 1,
+  },
+  description: {
+    flex: 2,
+  },
+  amount: {
+    flex: 1,
+    textAlign: "right",
+  },
+});
