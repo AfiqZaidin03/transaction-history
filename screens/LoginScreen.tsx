dc71d5243dbba153fe82b305c3b044ddf7de6d25
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-12-09 03:34:10 +0800

    [8] Add text input for login, function for login and style input with error

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,11 +1,44 @@
-// LoginScreen.tsx
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet, TextInput } from "react-native";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 
-const LoginScreen = () => {
+type LoginScreenProps = {
+  navigation: NavigationProp<any>;
+};
+
+const LoginScreen: React.FC<LoginScreenProps> = () => {
+  const navigation = useNavigation<NavigationProp<any>>();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    if (username === "User" && password === "p@ssw0rd") {
+      navigation.navigate("TransactionHistory");
+    } else {
+      setError("Invalid username or password");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+      <Text style={styles.title}>Transaction History</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      {error ? <Text style={styles.error}>{error}</Text> : null}
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 };
@@ -19,6 +52,18 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     marginBottom: 20,
+  },
+  input: {
+    width: "80%",
+    padding: 10,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+  },
+  error: {
+    color: "red",
+    marginBottom: 10,
   },
 });
 
