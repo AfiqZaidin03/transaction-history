e2819c6ce3afaab30b33ac15d6c083c591e92e2a
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-12-08 10:15:34 +0800

    [3] Create app file to add layout

---
+++ app/App.tsx
@@ -0,0 +1,14 @@
+// App.tsx
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import _layout from "./_layout";
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <_layout />
+    </NavigationContainer>
+  );
+};
+
+export default App;
